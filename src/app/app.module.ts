9b11580196fccf592b1d4a95544b66ca72ba9c66
geniu <zzreklama@gmail.com>
2019-01-21 17:19:00 +0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ConvertToSpacesPipe } from './shared/pipes/convert-to-space'
+import { InMemoryProductDataService } from './products/in-memory-product-data-service.service'
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { RouterModule, Routes } from '@angular/router'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { ProductsComponent } from './products/products.component'
+import { StarComponent } from './star/star.component'
+import { WelcomeComponent } from './welcome/welcome.component'
+import { ProductDetailComponent } from './products/product-detail.component'
 
+const appRoutes: Routes = [
+  { path: 'products-list', component: ProductsComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: '**', redirectTo: '/welcome', pathMatch: 'full' },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    WelcomeComponent,
+    ProductDetailComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryProductDataService, {
+      dataEncapsulation: false,
+    }),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
